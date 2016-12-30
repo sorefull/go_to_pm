63cbeee2df2001d612f02f4327ccada26a245257
sorefull <olegh.cherednichenko@gmail.com>
2016-12-30 12:53:00 +0200

    tests v1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
-  it { should validate_presence_of(:start_date) }
+  # it { should validate_presence_of(:start_date) }
 
   it { should have_many(:vacations) }
 
