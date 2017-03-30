e08cb78ce808668d4bb3c867e934c1850ee7425c
DimaSergienya <gunosada1@gmail.com>
2017-03-30 15:31:58 +0300

    Add role (status) for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230085902) do
+ActiveRecord::Schema.define(version: 20170330115440) do
 
   create_table "invitations", force: :cascade do |t|
     t.string   "secure_key"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 20161230085902) do
     t.string   "avatar"
     t.text     "comment"
     t.integer  "role",                   default: 0,  null: false
+    t.string   "status"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
